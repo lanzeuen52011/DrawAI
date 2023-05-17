bf02c4ac074d057cbe1c7374790c7421c2babb42
ZiYun Lan <lanzeuen52011@gmail.com>
2023-05-17 16:44:46 +0800

    Router 404

--- src/views/NotFoundComponent.vue
+++ src/views/NotFoundComponent.vue
@@ -5,13 +5,40 @@ export default {
 }
 </script>
 <template>
-  <div id="notPage">
+  <section id="notPage">
     <h1>404</h1>
+    <div>
+    <img src="@/image/404.png" alt="">
+    <p>404 好像沒有這個頁面唷...|</p>
+    <router-link class="About" to="/">點此回首頁</router-link>
     </div>
+    </section>
 </template>
 
 <style lang="scss" scoped>
-h1{
-    display: block;
+section{
+    margin-top: 3rem;
+}
+// h1{
+//     display: block;
+// }
+div{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+img{
+    width: 100px;
+    height: 100px;
+    object-fit: contain;
+    background-color: #e4e5e6;
+    border-radius: 50%;
+    margin-right: 1.5rem;
+    }
+h1,p,.About{
+    color: #e4e5e6;
+}
+.About:hover{
+    color: #fff;
 }
 </style>
